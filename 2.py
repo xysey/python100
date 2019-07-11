928645249323ae5f4bf9d553426e6f209796a7b7
xysey <xysey6@gmail.com>
2019-07-11 11:53:36 +0800

    I edited the comments for readability

--- 2.py
+++ 2.py
@@ -1,10 +1,12 @@
-# Question:
-# Write a program which can compute the factorial of a given numbers.
-# The results should be printed in a comma - separated sequence on a single line.
-# Suppose the following input is supplied to the program:
-# 8
-# Then, the output should be:
-# 40320
+'''
+Question:
+Write a program which can compute the factorial of a given numbers.
+The results should be printed in a comma - separated sequence on a single line.
+Suppose the following input is supplied to the program:
+8
+Then, the output should be:
+40320
+'''
 
 def fact(x):
     if x == 0:
